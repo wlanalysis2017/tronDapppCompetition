3a4dc4389c03e7faeeffa725ed0c4f35914ea974
Jeremy Rosenfeld <jeremy.rosenfeld@wailiantech.com>
2018-12-27 14:35:36 -0800

    update contract for resume id

--- contracts/Resume.sol
+++ contracts/Resume.sol
@@ -2,20 +2,20 @@ pragma solidity ^0.4.23;
 
 contract Resume {
 
-  mapping(uint => ResumeInfo) resumes;
+  mapping(bytes32 => ResumeInfo) resumes;
   struct ResumeInfo {
-    uint128 resumeID;
+    string resumeID;
     uint128 score;
     uint128 timestamp;
   }
 
-  function processResumes(uint128 resumeID, uint128 score, uint128 timestamp) public  {
-    resumes[resumeID] = ResumeInfo(resumeID, score, timestamp);
+  function processResumes(string resumeID, uint128 score, uint128 timestamp) public  {
+    resumes[keccak256(resumeID)] = ResumeInfo(resumeID, score, timestamp);
   }
 
-  function getResume(uint128 id) public view returns (uint128 resumeID, uint128 score, uint128 timestamp) {
-    ResumeInfo memory r = resumes[id];
+  function getResume(string id) public view returns (string resumeID, uint128 score, uint128 timestamp) {
+    ResumeInfo memory r = resumes[keccak256(id)];
     return (r.resumeID, r.score, r.timestamp);
-} 
+  } 
 
 }
