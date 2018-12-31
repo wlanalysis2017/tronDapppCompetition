9d667ae54512da79f661ca443346c7eab38c965f
Jeremy Rosenfeld <jeremy.rosenfeld@wailiantech.com>
2018-12-30 22:55:02 -0800

    update contract to store job title

--- contracts/Resume.sol
+++ contracts/Resume.sol
@@ -5,17 +5,18 @@ contract Resume {
   mapping(bytes32 => ResumeInfo) resumes;
   struct ResumeInfo {
     string resumeID;
+    string jobTitle; 
     uint128 score;
     uint128 timestamp;
   }
 
-  function processResumes(string resumeID, uint128 score, uint128 timestamp) public  {
-    resumes[keccak256(resumeID)] = ResumeInfo(resumeID, score, timestamp);
+  function processResumes(string resumeID, string jobTitle, uint128 score, uint128 timestamp) public  {
+    resumes[keccak256(resumeID)] = ResumeInfo(resumeID, jobTitle, score, timestamp);
   }
 
-  function getResume(string id) public view returns (string resumeID, uint128 score, uint128 timestamp) {
+  function getResume(string id) public view returns (string resumeID, string jobTitle, uint128 score, uint128 timestamp) {
     ResumeInfo memory r = resumes[keccak256(id)];
-    return (r.resumeID, r.score, r.timestamp);
+    return (r.resumeID, r.jobTitle, r.score, r.timestamp);
   } 
 
 }
