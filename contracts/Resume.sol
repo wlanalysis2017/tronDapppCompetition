b133316b212e83753fb0f0d317ec91125a679ee0
Jeremy Rosenfeld <jeremy.rosenfeld@wailiantech.com>
2019-01-04 09:46:07 -0800

    modify contract to view resumes

--- contracts/Resume.sol
+++ contracts/Resume.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.4.23;
-
+pragma experimental ABIEncoderV2;
 contract Resume {
 
   mapping(bytes32 => ResumeInfo) resumes;
+  mapping (address=>ResumeInfo[]) Map;
+
   struct ResumeInfo {
     string resumeID;
     string jobTitle; 
@@ -12,6 +14,7 @@ contract Resume {
 
   function processResumes(string resumeID, string jobTitle, uint128 score, uint128 timestamp) public  {
     resumes[keccak256(abi.encodePacked(resumeID))] = ResumeInfo(resumeID, jobTitle, score, timestamp);
+    Map[msg.sender].push(ResumeInfo(resumeID, jobTitle, score, timestamp));
   }
 
   function getResume(string id) public view returns (string resumeID, string jobTitle, uint128 score, uint128 timestamp) {
@@ -19,4 +22,7 @@ contract Resume {
     return (r.resumeID, r.jobTitle, r.score, r.timestamp);
   } 
 
+  function getResumes() public returns(ResumeInfo[]) {
+    return Map[msg.sender];
+  }
 }
